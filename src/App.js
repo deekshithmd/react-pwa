acf0b651a1a1349cde95c7c3c2dc0ce9eb9132a4
deekshithmd <deekshithmogra@gmail.com>
2023-05-28 17:52:44 +0530

    added counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
